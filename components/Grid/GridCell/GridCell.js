f2fe9baed470b23ccfe332467b1e49a842fd61a6
Niklas Grüner <niki.gruener@gmail.com>
2022-08-30 23:29:47 +0200

    Changed test image

--- components/Grid/GridCell/GridCell.js
+++ components/Grid/GridCell/GridCell.js
@@ -4,7 +4,13 @@ import styles from "./GridCell.module.css";
 export default function GridCell() {
   return (
     <div className={styles.cell}>
-      <img className={`${styles.cell_img} ${styles.curtain}`} width="100%" height="100%" src="https://www.leatherman.com/dw/image/v2/AAMV_PRD/on/demandware.static/-/Sites-master/default/dw5aab317e/large/basics-pullover-hoodie/0030_basics-pullover-hoodie-gray-front.jpg?sw=600&sh=600&sm=fit&sfrm=jpg" alt=""/>
+      <img
+        className={`${styles.cell_img} ${styles.curtain}`}
+        width="100%"
+        height="100%"
+        src="https://assets.manufactum.de/p/027/027480/27480_01.jpg/schneidbrett-bergahornholz.jpg"
+        alt=""
+      />
     </div>
   );
 }
